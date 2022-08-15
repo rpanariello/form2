ee6e7a0feca05eb48af31c74f88bcc70d3eb3291
Ross Panariello <ross@MacBook-Pro.local>
2022-08-15 11:14:36 -0400

    added js

---
+++ script1.js
@@ -0,0 +1,13 @@
+function showValue() {
+    let fname = document.getElementById('fname').value;
+    document.getElementById('ans').innerHTML = fname;
+   
+    let lname = document.getElementById('lname').value;
+    document.getElementById('ans1').innerHTML = lname; 
+    
+    let email = document.getElementById('email').value;
+    document.getElementById('ans2').innerHTML = email;
+}
+
+
+    
